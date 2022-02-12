40997c90961755f844e7712276027ec4526f6a6c
munyong <gjansdyd@gmail.com>
2022-02-12 15:21:44 +0900

    빌드 완료버전.

--- Pods/Target Support Files/Pods-wineFineThankyou-wineFineThankyouUITests/Pods-wineFineThankyou-wineFineThankyouUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_wineFineThankyou_wineFineThankyouUITests : NSObject
-@end
-@implementation PodsDummy_Pods_wineFineThankyou_wineFineThankyouUITests
-@end
