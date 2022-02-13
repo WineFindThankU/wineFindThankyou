f0af6cb4c8381d1a2cea80a5c90bc3b1bac59e42
suding <betty.sujin.lee@gmail.com>
2022-02-13 20:51:18 +0900

    [Add] Login Model 추가

---
+++ wineFineThankyou/Presentation/Login/LoginModel.swift
@@ -0,0 +1,19 @@
+//
+//  LoginModel.swift
+//  wineFindThankyou
+//
+//  Created by suding on 2022/02/13.
+//
+
+import Foundation
+enum LoginModel {
+    struct Request
+    {
+
+    }
+    struct Response
+    {
+        var statusCode: Int?
+        var message: String?
+    }
+}
