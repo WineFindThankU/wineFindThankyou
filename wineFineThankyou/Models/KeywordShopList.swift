2dd76e4b4753866c8914ec1d7ba090bafea03c82
sujin <betty.sujin.lee@gmail.com>
2022-02-27 23:01:12 +0900

    [Add] Request, Response 모델 생성

---
+++ wineFineThankyou/Models/KeywordShopList.swift
@@ -0,0 +1,13 @@
+//
+//  KeywordShopList.swift
+//  wineFindThankyou
+//
+//  Created by suding on 2022/02/27.
+//
+
+import Foundation
+
+struct KeywordShopList {
+    let type: String
+    let keyword: String
+}
---
+++ wineFineThankyou/Models/ShopList.swift
@@ -0,0 +1,16 @@
+//
+//  ShopList.swift
+//  wineFindThankyou
+//
+//  Created by suding on 2022/02/27.
+//
+
+import Foundation
+
+struct ShopList {
+    let type: String
+    let longtitude: Double
+    let latitude: Double
+    let category: String
+    let radius: Int
+}
