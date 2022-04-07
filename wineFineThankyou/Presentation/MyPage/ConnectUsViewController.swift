3d1651d42740f85b9c30499a5b01da05f35601be
munyong <gjansdyd@gmail.com>
2022-04-07 11:42:10 +0900

    1:1문의하기 title, 내용 다시 입력해야 하는 문제 수정. 취소 / 보내기 버튼을 눌렀을 때 delegate함수 구현.

--- wineFineThankyou/Presentation/MyPage/ConnectUsViewController.swift
+++ wineFineThankyou/Presentation/MyPage/ConnectUsViewController.swift
@@ -10,7 +10,7 @@ import Foundation
 import MessageUI
 
 
-class ConnectUsViewController: UIViewController, MFMailComposeViewControllerDelegate {
+class ConnectUsViewController: UIViewController {
     @IBOutlet weak var emailView: UIView!
     @IBOutlet weak var line: UIView!
     @IBOutlet weak var inquireView: UIView!
@@ -103,48 +103,8 @@ class ConnectUsViewController: UIViewController, MFMailComposeViewControllerDele
         txtView.layer.cornerRadius = 4
         txtView.font = UIFont.systemFont(ofSize: 13)
         txtView.textColor = UIColor(rgb: 0xbdbdbd)
-        txtView.textContainerInset = UIEdgeInsets(top: 5, left: 10, bottom: 5, right: 10);
+        txtView.textContainerInset = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5);
     }
-    
-    @objc
-    private func sendEmail() {
-        if MFMailComposeViewController.canSendMail() {
-                let composeViewController = MFMailComposeViewController()
-                composeViewController.mailComposeDelegate = self
-                
-                let bodyString = """
-                                 이곳에 내용을 작성해주세요.
-                                 -------------------
-                                 WineFindThankU
-                                 -------------------
-                                 """
-                
-                composeViewController.setToRecipients(["winefindthankyou@gmail.com"])
-                composeViewController.setSubject("[와인파인땡큐] 문의 및 의견")
-                composeViewController.setMessageBody(bodyString, isHTML: false)
-                
-                self.present(composeViewController, animated: true, completion: nil)
-            } else {
-                print("메일 보내기 실패")
-                let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패",
-                                                           message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
-                let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
-                    if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
-                        if #available(iOS 10.0, *) {
-                            UIApplication.shared.open(url, options: [:], completionHandler: nil)
-                        } else {
-                            UIApplication.shared.openURL(url)
-                        }
-                    }
-                }
-                let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
-                
-                sendMailErrorAlert.addAction(goAppStoreAction)
-                sendMailErrorAlert.addAction(cancleAction)
-                self.present(sendMailErrorAlert, animated: true, completion: nil)
-            }
-        }
-    
     
     @objc
     private func closeVC(){
@@ -184,3 +144,55 @@ extension ConnectUsViewController: UITextViewDelegate{
     }
 }
 
+extension ConnectUsViewController: MFMailComposeViewControllerDelegate{
+    @objc
+    private func sendEmail() {
+        if MFMailComposeViewController.canSendMail() {
+            let composeViewController = MFMailComposeViewController()
+            composeViewController.mailComposeDelegate = self
+            
+            let bodyString = """
+                                 \(inquireTxtView.text ?? "").
+                                 -------------------
+                                 WineFindThankU
+                                 -------------------
+                                 """
+            
+            composeViewController.setToRecipients(["winefindthankyou@gmail.com"])
+            composeViewController.setSubject("[와인파인땡큐] 문의 및 의견 - \(mailTitle ?? "")")
+            composeViewController.setMessageBody(bodyString, isHTML: false)
+
+            self.present(composeViewController, animated: true, completion: nil)
+        } else {
+            print("메일 보내기 실패")
+            let sendMailErrorAlert = UIAlertController(title: "메일 전송 실패",
+                                                       message: "메일을 보내려면 'Mail' 앱이 필요합니다. App Store에서 해당 앱을 복원하거나 이메일 설정을 확인하고 다시 시도해주세요.", preferredStyle: .alert)
+            let goAppStoreAction = UIAlertAction(title: "App Store로 이동하기", style: .default) { _ in
+                if let url = URL(string: "https://apps.apple.com/kr/app/mail/id1108187098"), UIApplication.shared.canOpenURL(url) {
+                    if #available(iOS 10.0, *) {
+                        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                    } else {
+                        UIApplication.shared.openURL(url)
+                    }
+                }
+            }
+            let cancleAction = UIAlertAction(title: "취소", style: .destructive, handler: nil)
+            
+            sendMailErrorAlert.addAction(goAppStoreAction)
+            sendMailErrorAlert.addAction(cancleAction)
+            self.present(sendMailErrorAlert, animated: true, completion: nil)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        DispatchQueue.main.async {
+            controller.dismiss(animated: true) {
+                switch result {
+                case .saved, .sent:
+                    self.dismiss(animated: true)
+                default: break;
+                }
+            }
+        }
+    }
+}
